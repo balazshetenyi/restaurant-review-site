6c9aec8d1e17ffdaa481dba609d7a5c0deaa2720
Balazs <Balazs@balazss-mbp.home>
2019-08-22 01:29:13 +0100

    removed public api key

--- src/Components/mapComponent/GoogleMap.js
+++ src/Components/mapComponent/GoogleMap.js
@@ -9,7 +9,7 @@ import ItemListFilter from '../itemListComponent/ItemListFilter'
 import MyModal from '../ModalComponent/Modal';
 
 // Google API Key
-const apiKey = "AIzaSyB544WTQdJXcToIRGRbIlJWWM4VlDOGck8"
+const apiKey = "Your API Key here..."
 
 function NewGoogleMap(props) {
     // Database
